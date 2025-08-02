13fbbc1e9c417634601d8221830f4b7415e7b14f
Bix <bix@bixed.xyz>
2025-08-02 19:03:25 +0100

    [Legacy] Update Noscript site

--- public/noscript/js/mobile-navbar.js
+++ public/noscript/js/mobile-navbar.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.querySelector('.hamburger')
-  const navMobile = document.querySelector('.nav-mobile')
+    const hamburger = document.querySelector('.hamburger');
+    const navMobile = document.querySelector('.nav-mobile');
 
-  hamburger.addEventListener('click', function () {
-    this.classList.toggle('active')
-    navMobile.classList.toggle('active')
-  })
+    hamburger.addEventListener('click', function () {
+        this.classList.toggle('active');
+        navMobile.classList.toggle('active');
+    });
 
-  // Close the menu when clicking a link
-  navMobile.querySelectorAll('a').forEach((link) => {
-    link.addEventListener('click', function () {
-      hamburger.classList.remove('active')
-      navMobile.classList.remove('active')
-    })
-  })
-})
+    // Fechar o menu ao clicar em um link
+    navMobile.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', function () {
+            hamburger.classList.remove('active');
+            navMobile.classList.remove('active');
+        });
+    });
+});
